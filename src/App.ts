375bd23303c1b4334b10d48bcfd77fe5690d2d97
sanshi <zhz961101@gmail.com>
2020-07-22 23:31:40 +0800

    :lipstick: 外链 top right

--- src/App.ts
+++ src/App.ts
@@ -6,37 +6,44 @@ import {
   useRef,
   useState,
   useStyle,
-} from "@tacopie/taco";
-import Editor from "./Editor";
-import Frame from "./Frame";
-import { _v } from "./utils";
-import defaultScript from "./defaultScript.txt";
+} from '@tacopie/taco';
+import Editor from './Editor';
+import Frame from './Frame';
+import { _v } from './utils';
+import defaultScript from './defaultScript.txt';
 
 const TopBtn = (props?, children?) => {
   const { color } = props || {};
 
   const { styleRef } = useStyle({
-    "font-weight": 100,
-    "color": `${_v(color) || "#cccbcc"}`,
-    "border": "0",
-    "outline": "none",
-    "padding": ".25rem .5rem",
-    "cursor": "pointer",
-    "background-color": "transparent",
-    "transition": ".1s all",
-    "&:active": {
-      color: "#fff",
-      background: "#37373f",
+    'font-weight': 100,
+    color: `${_v(color) || '#cccbcc'}`,
+    border: '0',
+    outline: 'none',
+    padding: '.25rem .5rem',
+    cursor: 'pointer',
+    'background-color': 'transparent',
+    transition: '.1s all',
+    '&:active': {
+      color: '#fff',
+      background: '#37373f',
     },
-    "&:hover": {
-      background: "#37373f",
+    '&:hover': {
+      background: '#37373f',
     },
   });
 
   return html`<button ref=${[styleRef]} ...${props}>
-    ${children || "options"}
+    ${children || 'options'}
   </button>`;
 };
+
+const TopLinkBtn = ({ link = '', title = '' } = {}, children?) => html` <${TopBtn}
+  title=${title || link}
+  onclick=${() => window.open(link, '_blank')}
+>
+  ${children}
+<//>`;
 
 const GithubBtn = () => {
   const elem = useRef(null);
@@ -45,31 +52,29 @@ const GithubBtn = () => {
     if (!el) {
       return;
     }
-    el.innerHTML = `<svg class="octicon octicon-mark-github v-align-middle" height="24" viewBox="0 0 16 16" version="1.1" width="24" aria-hidden="true" style="fill:#fff;"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>`
-  })
-  return html`<div ref=${elem}/>`
-}
+    el.innerHTML = `<svg class="octicon octicon-mark-github v-align-middle" height="24" viewBox="0 0 16 16" version="1.1" width="24" aria-hidden="true" style="fill:#fff;"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>`;
+  });
+  return html`<div ref=${elem} />`;
+};
 
 export const App = (props?, children?) => {
   const { styleRef } = useStyle({
-    "width": "100vw",
-    "height": "100vh",
-    "min-width": "640px",
-    "display": "flex", "flex-flow": "column",
-    "> header": {
-      "background-color": "rgb(30, 30, 30)",
-      "display": "flex",
+    width: '100vw',
+    height: '100vh',
+    'min-width': '640px',
+    display: 'flex',
+    'flex-flow': 'column',
+    '> header': {
+      'background-color': 'rgb(30, 30, 30)',
+      display: 'flex',
     },
-    "> div": {
-      flex: "auto",
-      overflow: "hidden",
-      display: "flex",
+    '> div': {
+      flex: 'auto',
+      overflow: 'hidden',
+      display: 'flex',
     },
   });
-  const content = useLocalState(
-    "@tacopia/taco-playground",
-    defaultScript,
-  );
+  const content = useLocalState('@tacopia/taco-playground', defaultScript);
   const [, , editContent] = useState(() => content.value);
   const syncContent = () =>
     // 这里是为了截断依赖捕获的逻辑，后续会提供 skip 元语
@@ -79,12 +84,18 @@ export const App = (props?, children?) => {
   return html`
     <div ref=${[styleRef]}>
       <header>
-        <${TopBtn}><${Icon} name="whatshot" /><//>
+        <${TopBtn} title="Tacojs-playground"><${Icon} name="whatshot" /><//>
         <${TopBtn}>File<//>
         <${TopBtn}>Edit<//>
-        <${TopBtn} color=${"rgb(45, 181, 93)"} onclick=${refrash}>Run<//>
+        <${TopBtn} color=${'rgb(45, 181, 93)'} onclick=${refrash}>Run<//>
         <${TopBtn}>Help<//>
-        <${TopBtn} onclick=${() => window.open('https://github.com/zhzLuke96/tacojs-playground', '_blank')}><${GithubBtn} /><//>
+        <div style=${{ flex: 1, margin: 'auto' }}></div>
+        <${TopLinkBtn} link="https://github.com/zhzLuke96/TacoJs">
+          🌮
+        <//>
+        <${TopLinkBtn} link="https://github.com/zhzLuke96/tacojs-playground">
+          <${GithubBtn} />
+        <//>
       </header>
       <div>
         <${Editor}
