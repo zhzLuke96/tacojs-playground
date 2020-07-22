4ba89e195e841a42c16ae447e48d3d7059dfe486
sanshi <zhz961101@gmail.com>
2020-07-22 23:06:43 +0800

    :bento: github link.

--- src/App.ts
+++ src/App.ts
@@ -12,7 +12,7 @@ import Frame from "./Frame";
 import { _v } from "./utils";
 import defaultScript from "./defaultScript.txt";
 
-const RunBtn = (props, children) => {
+const TopBtn = (props?, children?) => {
   const { color } = props || {};
 
   const { styleRef } = useStyle({
@@ -38,7 +38,19 @@ const RunBtn = (props, children) => {
   </button>`;
 };
 
-export const App = (props, children) => {
+const GithubBtn = () => {
+  const elem = useRef(null);
+  useEffect(() => {
+    const [el] = [elem.value];
+    if (!el) {
+      return;
+    }
+    el.innerHTML = `<svg class="octicon octicon-mark-github v-align-middle" height="24" viewBox="0 0 16 16" version="1.1" width="24" aria-hidden="true" style="fill:#fff;"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>`
+  })
+  return html`<div ref=${elem}/>`
+}
+
+export const App = (props?, children?) => {
   const { styleRef } = useStyle({
     "width": "100vw",
     "height": "100vh",
@@ -67,11 +79,12 @@ export const App = (props, children) => {
   return html`
     <div ref=${[styleRef]}>
       <header>
-        <${RunBtn}><${Icon} name="whatshot" /><//>
-        <${RunBtn}>File<//>
-        <${RunBtn}>Edit<//>
-        <${RunBtn} color=${"rgb(45, 181, 93)"} onclick=${refrash}>Run<//>
-        <${RunBtn}>Help<//>
+        <${TopBtn}><${Icon} name="whatshot" /><//>
+        <${TopBtn}>File<//>
+        <${TopBtn}>Edit<//>
+        <${TopBtn} color=${"rgb(45, 181, 93)"} onclick=${refrash}>Run<//>
+        <${TopBtn}>Help<//>
+        <${TopBtn} onclick=${() => window.open('https://github.com/zhzLuke96/tacojs-playground', '_blank')}><${GithubBtn} /><//>
       </header>
       <div>
         <${Editor}
